2d9d7b3951b7835a5a678390451466d424d1edbf
Carolina <carolinanobre@gmail.com>
2013-09-16 16:36:09 -0400

    First version of P1

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>P1t</title>
+<title>P1 E-15</title>
 </head>
 
 <body>
+P1
 
-Welcome to P1
+<p>Name: Carolina Nobre</p>
+
+<p>Bio: I work at the Woods Hole Oceanographic Institution as a Research Associate. I work mainly with Matlab to process the data that is acquired on the cruises, mainly in Physical Oceanography. </p>
+
+<p>Editor: Dreamweaver </p>
+
 </body>
 </html>
