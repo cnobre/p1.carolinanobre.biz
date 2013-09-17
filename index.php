b17f97bf1422d978855b88681c423e9ac3f860d9
Carolina <carolinanobre@gmail.com>
2013-09-17 17:27:42 -0400

    Latest update to P1 submission

--- index.php
+++ index.php
@@ -1,18 +1,61 @@
-<!DOCTYPE HTML5">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>P1 E-15</title>
+<meta charset="UTF-8">
+<title>P1 - Carolina Nobre</title>
+<link rel="stylesheet" type="text/css" href="styles.css"/>
 </head>
-
 <body>
-P1
-
-<p>Name: Carolina Nobre</p>
-
-<p>Bio: I work at the Woods Hole Oceanographic Institution as a Research Associate. I work mainly with Matlab to process the data that is acquired on the cruises, mainly in Physical Oceanography. </p>
-
-<p>Editor: Dreamweaver </p>
-
-</body>
+   <div class="bg">
+    <!--start container-->
+    <div id="container">
+    <!--start header-->
+    <header>
+      <!--start logo-->
+      <a href="#" id="logo"><img src="images/logo.png" width="345" height="69" alt="logo"/></a>    
+      <!--end logo-->
+     
+      <!--end header-->
+	</header>
+   <!--start intro-->
+   <section id="intro">
+      <hgroup>
+      <h1>"I love deadlines. I like the whooshing sound they make as they fly by."<span>- Douglas Adams</span></h1>
+      <h2></h2>
+      </hgroup>
+   </section>
+   <!--end intro-->
+   <!--start holder-->
+   <div class="holder_content">
+      <section class="group1">
+         <h3>About Me</h3>
+         <p>Originally from Brazil, I moved to the US in 2008 to get my master's degree in Oceanography. While doing so, I fell in love with country music, became a vegetarian, and realized my true passion lies in programming and computer science.</p><br/>
+         <a class="photo_hover3" href="#"><img src="images/picture1.jpg" width="240" height="214" alt="picture1"/></a>
+        
+       </section>
+      <section class="group2">
+         <h3>My Job</h3>
+         <p>After earning my degree I had a brief stint at a company making current predictions for  oil and gas companies. Today I work in the physical oceanography deptartment at WHOI (Woods Hole Oceanographic Institution) processing data and writing software for data acquired on the cruises.</p>
+         <a class="photo_hover3" href="#"><img src="images/picture3.jpg" width="240" height="214" alt="picture1"/></a>
+      
+       </section>
+      <section class="group3">
+         <h3>If I had to do it over...</h3>
+         <p>Definitely would have gone to undergrad in Computer Science, or perhaps Photography. Although I must say that Oceanography has allowed me to travel more than I ever would have in a CS career. Over the past 2 years I have been to the Arctic, Norway, Bermuda, Turkey, Iceland...</p> 
+         <a class="photo_hover3" href="#"><img src="images/picture2.jpg" width="240" height="214" alt="picture1"/></a>
+          
+   	</section>
+	</div>
+	<!--end holder-->
+   
+   </div>
+   <!--end container-->
+   <!--start footer-->
+   <footer>
+     
+   </footer>
+   <!--end footer-->
+   </div>
+   <!--end bg-->
+  </body>
 </html>
